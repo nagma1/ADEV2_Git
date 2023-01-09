8dc452001884d20fd41c2eb993b89b79ded33ef3
nagma1 <nagma@rv-vlsi.com>
2023-01-09 15:05:18 +0700

    Added 2 files

--- temp123.c
+++ temp123.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 void add(int,int);
 void add(int a,int b){
-  printf("Sum=%d\n",a+b);
+  printf("Sum=%d\n",a_b);
 }
 
 int main(){
   printf("Welcome to GIT\n");
-  add(100+200);
   sreturn 0;
 }
